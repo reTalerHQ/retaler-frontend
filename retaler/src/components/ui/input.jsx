a6565056b9a71b38c889d61f0868e5c210d35d4b
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2025-07-12 16:40:57 +0100

    add: responsive layout page with header and sidebar

--- retaler/src/components/ui/input.jsx
+++ retaler/src/components/ui/input.jsx
@@ -1,24 +1,75 @@
-import * as React from "react"
+import React, { forwardRef } from "react";
+import { cn } from "@/lib/utils";
+import { Label } from "./label";
 
-import { cn } from "@/lib/utils"
+const Input = forwardRef(
+  (
+    {
+      className,
+      type = "text",
+      leftIcon,
+      rightIcon,
+      label,
+      error,
+      id,
+      showError = true,
+      ...props
+    },
+    ref,
+  ) => {
+    const handleWheel = (event) => {
+      if (type === "number") {
+        event.currentTarget.blur();
+      }
+    };
 
-function Input({
-  className,
-  type,
-  ...props
-}) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props} />
-  );
-}
+    const handleKeyDown = (event) => {
+      if (
+        type === "number" &&
+        (event.key === "ArrowUp" || event.key === "ArrowDown")
+      ) {
+        event.preventDefault();
+      }
+    };
 
-export { Input }
+    return (
+      <div className="flex w-full flex-col gap-3">
+        {label ? <Label htmlFor={id}>{label}</Label> : null}
+        <div className="relative">
+          {leftIcon ? (
+            <span className="text-fade absolute top-[1.75px] bottom-[1.75px] left-[1.5px] z-10 flex items-center justify-center">
+              {leftIcon}
+            </span>
+          ) : null}
+          <input
+            type={type}
+            className={cn(
+              `focus-visible:ring-primary flex h-9.5 w-full rounded-sm border border-[#BBBBBB] px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-300 focus:border-none focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 ${
+                leftIcon ? "pl-5" : ""
+              } ${rightIcon ? "pr-5" : ""}`,
+              type === "number" && "hide-number-arrows",
+              className,
+            )}
+            ref={ref}
+            id={id}
+            onWheel={handleWheel}
+            onKeyDown={handleKeyDown}
+            {...props}
+          />
+          {rightIcon ? (
+            <span className="text-fade absolute top-[50%] right-1 z-20 -translate-y-[50%]">
+              {rightIcon}
+            </span>
+          ) : null}
+        </div>
+        {showError && (
+          <p className="mt-0.5 h-1 text-[10px] text-red-500">{error}</p>
+        )}
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export { Input };
