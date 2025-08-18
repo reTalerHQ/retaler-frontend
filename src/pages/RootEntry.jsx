f7b263550207749c71235969b4b43dd7a4745790
Nwachukwu Uzor <66738486+Nwachukwu-Uzor@users.noreply.github.com>
2025-08-18 15:54:26 +0100

    Feature/vercel rewrite (#40)
    * update rewrite to index html page
    
    * update vercel json file for rewrite
    
    * update vite config to point to dist folder as build
    
    * update vercel.json file to add static build configuration
    
    * fix: update vercel.json for SPA routing
    
    * update navigation on splash screen page
    
    ---------
    
    Co-authored-by: ¨Nwachukwu-Uzor¨ <¨uzor1997@gmail.com¨>

--- src/pages/RootEntry.jsx
+++ src/pages/RootEntry.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import { useIsMobile } from "@/hooks/use-is-mobile";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { SplashScreen } from "./splashScreen";
 
 export const RootEntry = () => {
   const isMobile = useIsMobile();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isMobile) {
-      navigate("/welcome", { replace: true });
-    }
-  }, [isMobile, navigate]);
 
   if (isMobile) {
     return <SplashScreen />;
   }
 
-  return null;
+  return <Navigate to="/welcome" replace />;
 };
