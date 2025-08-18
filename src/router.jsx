45e9250dfd6769fcc05df770a7f170983fa659e5
Nwachukwu Uzor <66738486+Nwachukwu-Uzor@users.noreply.github.com>
2025-08-18 16:08:05 +0100

    revert back to browser router (#42)
    Co-authored-by: ¨Nwachukwu-Uzor¨ <¨uzor1997@gmail.com¨>

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { DashboardLayout } from "@/layouts/dashboard-layout";
 import {
   AnalyticsPage,
@@ -48,7 +48,7 @@ import { AddNewSales } from "@/pages/add-new-sales";
 
 import { Bus } from "lucide-react";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootEntry />,
