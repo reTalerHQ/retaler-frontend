902a722b514f1db3bb3613cb10cf33f563e6f328
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2025-09-13 19:31:15 +0100

    implement google sign up and sign in

--- src/pages/google-callback.jsx
+++ src/pages/google-callback.jsx
@@ -1,50 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import axiosInstance from "@/lib/axios";
 import { BASE_URL } from "@/constants/api";
-import { TOKEN_IDENTIFIER, USER_INFO_KEY, REFRESH_TOKEN_IDENTIFIER } from "@/constants";
+import {
+  TOKEN_IDENTIFIER,
+  USER_INFO_KEY,
+  REFRESH_TOKEN_IDENTIFIER,
+} from "@/constants";
 import { toast } from "sonner";
+import { jwtDecode } from "jwt-decode";
+import axiosInstance from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query";
+import { PagePreLoader } from "@/components/page-pre-loader";
 
 const GoogleCallback = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const handleGoogleCallback = async () => {
+  const { mutate: handleGoogleResponse, isPending } = useMutation({
+    mutationFn: async () => {
       const searchParams = new URLSearchParams(location.search);
-      const code = searchParams.get("code");
+      const accessToken = searchParams.get("access_token");
+      const refreshToken = searchParams.get("refresh_token");
 
-      if (code) {
-        try {
-          const rsp = await axiosInstance.post(
-            `${BASE_URL}/v1/users/google-auth/callback`,
-            { code },
-          );
-
-          const {
-            token: { access_token, refresh_token },
-            user,
-          } = rsp.data;
-
-          sessionStorage.setItem(TOKEN_IDENTIFIER, access_token);
-          sessionStorage.setItem(REFRESH_TOKEN_IDENTIFIER, refresh_token);
-          sessionStorage.setItem(USER_INFO_KEY, JSON.stringify(user));
-
-          navigate("/dashboard");
-        } catch (error) {
-          console.log({ error });
-          const message =
-            error?.response?.data?.detail ?? "Something went wrong...";
-          toast.error(message);
-          navigate("/sign-in");
-        }
+      if (!accessToken || !refreshToken) {
+        toast.error("Google authenticatication failed");
+        return;
       }
-    };
 
-    handleGoogleCallback();
-  }, [location, navigate]);
+      try {
+        sessionStorage.setItem(TOKEN_IDENTIFIER, accessToken);
+        sessionStorage.setItem(REFRESH_TOKEN_IDENTIFIER, refreshToken);
+        toast.success("Google authentication complete");
+        const tokenInfo = jwtDecode(accessToken);
 
-  return <div>Google Auth.</div>;
+        const response = await axiosInstance.get(
+          `${BASE_URL}/v1/users/${tokenInfo.id}`,
+        );
+        const user = response?.data?.user;
+
+        if (!user) {
+          toast.error("Google auth failed");
+          return;
+        }
+
+        sessionStorage.setItem(USER_INFO_KEY, JSON.stringify(user));
+
+        navigate("/dashboard");
+        return;
+      } catch (error) {
+        console.log({ error });
+        const message = error.response.data.detail;
+        toast.error(message ?? "Something went wrong...");
+      }
+    },
+  });
+
+  useEffect(() => {
+    handleGoogleResponse();
+  }, [handleGoogleResponse]);
+
+  return <>{isPending && <PagePreLoader />}</>;
 };
 
 export default GoogleCallback;
